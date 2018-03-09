343e22bca8d4d0facf1d92fabe4d4a47f77acdc5
EngLlif <2807412339@qq.com>
2018-03-09 15:13:37 +0800

    My First Commit

---
+++ src/main/java/domain/test.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class test {
+    private Integer id;
+
+    public test() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
